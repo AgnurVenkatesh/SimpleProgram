d1726bc70a29856694cd356f5da395a866ed4ce1
AgnurVenkatesh <agnurvenkatesh999@gmail.com>
2022-03-15 20:19:32 +0530

    first commit

---
+++ SimplePro/src/main/java/com/lti/demos/SimplePro.java
@@ -0,0 +1,14 @@
+package com.lti.demos;
+
+public class SimplePro {
+
+	public static void main(String[] args) 
+	{
+		int x=50;
+		int y=180;
+		int res1=y/x;
+		int res2=y%x;
+		System.out.println("Quotient of "+x+" and "+y+" is: "+res1);
+		System.out.println("Remainder of "+x+" and "+y+" is: "+res2);
+	}
+}
